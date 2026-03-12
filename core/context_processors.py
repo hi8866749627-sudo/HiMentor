344f3963ab8c479b5af88d763a99be52012afa3d
erhardik <er.hardik9190@.com>
2026-03-13 01:11:20 +0530

    Show mentor full name in header

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,5 @@
 from .module_utils import allowed_modules_for_user, get_current_module, is_superadmin_user
+from .utils import resolve_mentor_identity
 
 
 def module_context(request):
@@ -14,6 +15,7 @@ def module_context(request):
             "current_module": None,
             "can_manage_modules": False,
             "home_url": home_url,
+            "mentor_display_name": "",
         }
 
     current = get_current_module(request)
@@ -21,10 +23,18 @@ def module_context(request):
     current_id = current.id if current else None
     for m in modules:
         m.is_current = (m.id == current_id)
+
+    mentor_display_name = ""
+    if request.session.get("mentor"):
+        mentor_obj = resolve_mentor_identity(request.session.get("mentor"))
+        if mentor_obj:
+            mentor_display_name = mentor_obj.full_name or mentor_obj.name
+        else:
+            mentor_display_name = request.session.get("mentor")
     return {
         "module_list": modules,
         "current_module": current,
         "can_manage_modules": bool(request.user.is_authenticated and not request.session.get("mentor") and is_superadmin_user(request.user)),
         "home_url": home_url,
+        "mentor_display_name": mentor_display_name,
     }
-
